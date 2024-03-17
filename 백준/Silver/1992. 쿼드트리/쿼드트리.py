eff4776546947cc5b4556ed9d4d89cb97b170095
개발작 <97375085+raymondkang4837@users.noreply.github.com>
2024-03-17 18:02:37 +0900

    [Silver I] Title: 쿼드트리, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -0,0 +1,20 @@
+n = int(input())
+nums = [input() for _ in range(n)]
+
+def paper(nums,n):
+    cnt = 0
+    for i in nums:
+        cnt += i.count('1')
+    if cnt == n**2:
+        print(1,end='')
+        return
+    elif cnt == 0:
+        print(0,end='')
+        return
+    print('(',end='')
+    paper([nums[i][:n//2] for i in range(n//2)],n//2)
+    paper([nums[i][n//2:] for i in range(n//2)],n//2)
+    paper([nums[i][:n//2] for i in range(n//2,n)],n//2)
+    paper([nums[i][n//2:] for i in range(n//2,n)],n//2)
+    print(')',end='')
+paper(nums,n)
