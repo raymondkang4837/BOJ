62508e1d17319cfce29c3ff602f29780857c431a
개발작 <97375085+raymondkang4837@users.noreply.github.com>
2025-03-14 23:32:14 +0900

    [Gold II] Title: 트리의 지름, Time: 560 ms, Memory: 86908 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,43 +1,52 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10 ** 9)
+
+# BeakJoon 1167
+
 input = sys.stdin.readline
+n = int(input())
+nums = [[] for _ in range(n + 1)]
 
-V = int(input())
-tree = [[] for _ in range(V+1)]
-# 2차원 리스트에 트리 저장(연결 그래프)
-for _ in range(V):
-    line = list(map(int, input().split()))
-    cnt_node = line[0]
-    idx = 1
-    while line[idx] != -1:
-        adj_node, adj_cost = line[idx], line[idx+1]
-        tree[cnt_node].append((adj_node, adj_cost))
-        idx += 2
+# Graph
+for _ in range(n):
+    i = 1
+    for x in map(int, input().split()):
+        if x == -1 :
+            break
+        if i == 1:
+            p = x
+            i = 2
+        elif i == 2:
+            k = x
+            i -= 2
+        else:
+            nums[p].append((k,x))
+            i += 2
 
-def BFS(start):
-    q = deque()
-    q.append((start, 0))
-    visited = [-1]*(V+1)
-    visited[start] = 0
-    res = [0, 0] # start로부터 가장 먼 거리에 있는 노드와 거리 값
-    
-    while q:
-        cnt_node, cnt_dist = q.popleft()
-        
-        for adj_node, adj_dist in tree[cnt_node]:
-            if visited[adj_node] == -1:
-                cal_dist = cnt_dist + adj_dist
-                q.append((adj_node, cal_dist))
-                visited[adj_node] = cal_dist
-                if res[1] < cal_dist:
-                    res[0] = adj_node
-                    res[1] = cal_dist
-        
-    return res
-    
-# 트리 지름 공식 참고
-# u-v가 지름이라고 하자. 임의의 점 x에서 가장 먼 거리의 노드 y는
-# 반드시 u 또는 v이다. 따라서 y에서 BFS를
-# 한번 더 해주면 지름을 구할 수 있다.
-point, _ = BFS(1)
-print(BFS(point)[1])
+
+# Checking
+visited = [-1]*(n+1)
+arr = [1]
+visited[1] = 0
+ans = []
+
+def dfs_cost_checking(node, cost):
+    for son, weight in nums[node]:
+        total = cost + weight
+        if visited[son] == -1: # 끝
+            visited[son] = total
+            dfs_cost_checking(son, total)
+
+dfs_cost_checking(1,0)
+tmp = [0,0]
+for i in range(1,len(visited)):
+    if visited[i] > tmp[1]:
+        tmp[1] = visited[i]
+        tmp[0] = i
+
+visited = [-1] * (n+1)
+visited[tmp[0]] = 0
+dfs_cost_checking(tmp[0],0)
+
+print(max(visited))
